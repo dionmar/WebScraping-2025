497631ad20e3f3791f3806327baf5a1f3428b79e
Dion Mar <dion.mar@gmail.com>
2025-04-10 00:23:57 -0300

    Scrapping ML

---
+++ coleta/coleta/spiders/notebook.py
@@ -0,0 +1,19 @@
+import scrapy
+
+
+class NotebookSpider(scrapy.Spider):
+    name = "notebook"
+    allowed_domains = ["lista.mercadolivre.com.br"]
+    start_urls = ["https://lista.mercadolivre.com.br/notebook?sb=rb#D[A:notebook]"]
+
+    def parse(self, response):
+        products = response.css('div.ui-search-result__wrapper')
+        
+        for product in products:
+            
+            yield {
+                'brand': product.css('span.poly-component__brand::text').get(),
+                'name': product.css('a.poly-component__title::text').get()
+            }
+            
+            pass
